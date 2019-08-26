77e5eecf80e53636cb725514213c0134ea20c76d
Johnny Gu <johnny@wiredcraft.com>
2019-08-26 18:28:56 +0800

    connecting to mongodb

--- index.js
+++ index.js
@@ -1,63 +1,75 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
 const mailer = require('./lib/mail/mailer')
+const series = require('./lib/mongod/series')
+const listener = require('./lib/mongod/listeners')
+const spider = require('./lib/ttmj_spider')
 
-const mailTo = 'guqiang180@gmail.com'
-const baseURL = 'http://www.ttzmz.vip/meiju'
-const inputName = "Agents of S H I E L D"
-const season = 6
+const seriesUpdateCheck = () => {
+	return series.read().then(values => {
+		values.client.close()
+		const jobs = values.result.map(record => {
+			return spider(record.name, record.season)
+				.then(fetched => {
+					const newEposids = fetched.epsoids.filter(e => record.epsoids.find(re => re.fileName === e.fileName) === 0)
+					const updatedRecrod = {
+						original: record,
+						newEposids
+					}
+					console.log(updatedRecrod)
+					return Promise.resolve(fetched.epsoids.length == record.epsoids.length ? updatedRecrod : null)
+				})
+		})
+		return Promise.all(jobs).then(records => {
+			return Promise.resolve(records.filter(a => a !== null))			
+		})
+	})
+}
 
-const concateRequestURL = (inputName, season) => {
-	let formatedName = inputName
-	formatedName = formatedName.replace(' ', '.')
-	if (season) {
-		return `${baseURL}/${formatedName}/${season}.html`
-	} else {
-		return `${baseURL}/${formatedName}.html`
-	}
+const addListener = (mail, name, season) => {
+	const updateListener = listener.read({mail}).then(values => {
+		values.client.close()
+		const result = values.result
+		if (result.length > 0) {
+			const listened = result.listenTo.filter(l => l.name == name && l.season).length > 0
+			if (listened) throw new Error('Already added to the listen list, no need to add again')
+			return listener.update({mail, listenTo: result.listenTo.slice().push({name, season})})
+		} else {
+			return listener.update({mail, listenTo: [name]})
+		}
+	}).then(values => {
+		values.client.close()
+	})
+
+	const updateSeries = series.read({name, season}).then(values => {
+		values.client.close()
+		const result = values.result
+		if (result.length === 0) {
+			return spider(name, season)
+		} else {
+			Promise.resolve(result.epsoids)
+		}
+	}).then(seedlist => {
+		// insert to the db
+		return series.update({
+			name,
+			season,
+			epsoids: seedlist
+		})
+	}).then(values => {
+		values.client.close()
+		// send email
+		return mailer(name, values.result.epsoids, mail)
+	})
+
+	return Promise.all([updateListener, updateSeries])
+}
+
+const dailyCheck = () => {
+
 }
 
 const index = async () => {
-	const url = concateRequestURL(inputName, season)
-	const res = await axios.get(url)
-	const $ = cheerio.load(res.data)
-
-	const updateTimeRaw = $('div.seedlink span:nth-child(4)').text()
-	const updateTime = new Date(updateTimeRaw.split('：').pop())
-
-	const seedlist = $('tbody#seedlist').children().map((i, el) => {
-		const tds = $(el).children()
-		const fileName = tds.eq(1).find('font').text()
-
-		const downloads = tds.eq(2).children().map((i, el) => {
-			const title = $(el).attr('title')
-			let link = $(el).attr('href')
-			if (title.includes('迅雷')) {
-				link = $(el).attr('data-url')
-			}
-			return {
-				title,
-				link
-			}
-		}).get()
-
-		const baiduCode = tds.eq(4).text()
-		const size = tds.eq(5).text()
-		const format = tds.eq(6).text()
-		const zimu = tds.eq(7).children().text()
-		const updateAt = tds.eq(8).text()
-		return {
-			fileName,
-			downloads,
-			baiduCode,
-			size,
-			format,
-			zimu,
-			updateAt
-		}
-	}).get()
-
-	mailer(inputName, seedlist, mailTo)
+	const result = await seriesUpdateCheck()
+	console.log(result)
 }
 
 index().catch(console.error)
